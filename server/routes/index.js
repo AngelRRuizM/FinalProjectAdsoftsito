7d571812100edd13d8dd69eab9c09d478e6c95e7
Carlos2396 <carlosaam96@gmail.com>
2018-09-11 18:22:25 -0500

    Created user controller with create, get, list, update and delete methods. Added user crud routes.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,12 @@
 module.exports = (app) => {
-    app.get('/api', (req, res) => res.status(200).send({
-      message: 'Welcome to API!',
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to API!',
   }));
+
+  //Routes for the USERS table
+  app.post('/api/users', usersController.create);
+  app.get('/api/users', usersController.list);
+  app.get('/api/users/:id', usersController.retrieve);
+  app.put('/api/users/:id', usersController.update);
+  app.delete('/api/users/:id', usersController.destroy);
 }
